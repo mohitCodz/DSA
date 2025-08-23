a433ec764adbe9b2710a5d7c07b3b95f12e4604a
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-23 12:50:39 +0530

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,1 +1,35 @@
 // leet code problem number 1
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+
+    return 0;
+}
