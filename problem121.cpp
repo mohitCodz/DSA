4aab583caa7a8c9176257458f79f82431dfccbb5
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-24 18:18:54 +0530

    Update problem121.cpp

--- problem121.cpp
+++ problem121.cpp
@@ -2,10 +2,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxProfit = 0, bestBuy = prices[0];
-        for(int i = 1; i < prices.size();i++){
-            if(prices[i] > bestBuy)
-            maxProfit = max(maxProfit,prices[i] -  bestBuy);
+        int maxProfit = 0, bestBuy = prices[0]; // best buy stores minimun price to buy the stock
+        for(int i = 1; i < prices.size();i++){ 
+            if(prices[i] > bestBuy) // selling today could make a profit.
+            maxProfit = max(maxProfit,prices[i] -  bestBuy); 
             bestBuy = min(bestBuy, prices[i]);
         }return maxProfit;
     }
