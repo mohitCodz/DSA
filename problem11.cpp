d6efb8eb7cefa57fcd72927e400c0f43bccd40a7
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-04 16:54:21 +0530

    Update problem11.cpp

--- problem11.cpp
+++ problem11.cpp
@@ -2,18 +2,18 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-       for (itn i = 0; i < n; i++)
-       {
-       for (int j = i+1; j < n; j++)
-       {
-       int width = j-1; // width
-       height = min(height[i],height[j]);
-       int area = width * height;
-       maxArea = max(maxArea,area);
-       }
-       
-       }
-       return maxArea;
-        
+        int n = height.size();
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int width = j - i; 
+                int h = min(height[i], height[j]); 
+                int area = width * h;
+                maxArea = max(maxArea, area);
+            }
+        }
+
+        return maxArea;
     }
 };
