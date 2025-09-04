3592ddb7366df3c58f4bc570aaa665be10ebfbdf
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-04 17:28:47 +0530

    Update problem11.cpp

--- problem11.cpp
+++ problem11.cpp
@@ -1,18 +1,21 @@
 // leetCode problem number 11 - container with most water
 
-// 2 Pointers Approach
-
-
-
-
-
-
-
-
-
-
-
-
+// 2 Pointers Approach - Optimal Solution
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+      int l=0,currWater=0,maxWater=0;
+      int r = height.size()-1;
+      while(l<r){
+      int w = r - l;
+      int h = min(height[l],height[r]);
+      currWater = w * h;
+maxWater = max(maxWater,currWater);
+height[l] < height[r] ? l++:r--;
+      }
+      return maxWater;
+    }
+};
 
 /* 
 using Brute Force Approach
