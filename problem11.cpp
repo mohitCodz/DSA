33736b86adf4da511ece91b0dd1fa8da0209f3ba
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-04 16:49:52 +0530

    Update problem11.cpp

--- problem11.cpp
+++ problem11.cpp
@@ -1,1 +1,15 @@
 // leetCode problem number 11 - container with most water
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       for (itn i = 0; i < n; i++)
+       {
+       for (int j = i+1; j < n; j++)
+       {
+        /* code */
+       }
+       
+       }
+        
+    }
+};
