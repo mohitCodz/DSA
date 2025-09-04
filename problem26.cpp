b8b0956c93ef79c4b7733f89fe6b62a7c22bf60a
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-04 18:00:17 +0530

    Update problem26.cpp

--- problem26.cpp
+++ problem26.cpp
@@ -1,1 +1,14 @@
 // leetCode  question 26 - Remove Duplicates from Sorted Array using 2 Pointer approach
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int lastUnique = 0; 
+        for(int i =1;i<nums.size();i++){
+            if(nums[i] != nums[lastUnique]) lastUnique++;
+            nums[lastUnique] = nums[i];
+        }
+        return lastUnique +1;
+    }
+};
