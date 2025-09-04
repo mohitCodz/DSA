13515e281a8eae501579e7767cc290e95a65a66c
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-04 18:00:34 +0530

    Update problem26.cpp

--- problem26.cpp
+++ problem26.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if(nums.empty()) return 0;
-        int lastUnique = 0; // stores
+        int lastUnique = 0; // stores last unique element
         for(int i =1;i<nums.size();i++){
             if(nums[i] != nums[lastUnique]) lastUnique++;
             nums[lastUnique] = nums[i];
