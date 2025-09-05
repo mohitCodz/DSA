636396ac6b64172b463377b10d5537b1d43ec3ee
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-05 21:34:47 +0530

    Update problem238.cpp

--- problem238.cpp
+++ problem238.cpp
@@ -2,19 +2,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+                int n = nums.size();
         vector<int> ans(n,1);
-         vector<int> pefix(n,1);
-          vector<int> sufix(n,1);
 
-          // prefix
           for(int i =1;i<n;i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
           }
-
-          // sufix
+            int suffix =1;
           for(int i =n-2;i>=0;i--){
-            sufix[i] = sufix[i+1]*nums[i+1];
+             suffix *= nums[i+1]; // calculating ith suffix
+           ans[i]*= suffix;
           }
+        
           return ans;
     }
 };
