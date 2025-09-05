ff5a574c4d1f4056929d560810637895e0dc8145
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-05 20:57:27 +0530

    Update problem238.cpp

--- problem238.cpp
+++ problem238.cpp
@@ -1,1 +1,20 @@
 // Product of Array Except Self - leetode question 238 
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(n,1);
+         vector<int> pefix(n,1);
+          vector<int> sufix(n,1);
+
+          // prefix
+          for(int i =1;i<n;i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
+          }
+
+          // sufix
+          for(int i =n-2;i>=0;i--){
+            sufix[i] = sufix[i+1]*nums[i+1];
+          }
+          return ans;
+    }
+};
