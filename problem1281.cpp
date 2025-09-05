f3551d85d6ec1f5d602d0ddfd45f1f3c76de4d33
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-06 00:49:11 +0530

    Update problem1281.cpp

--- problem1281.cpp
+++ problem1281.cpp
@@ -5,7 +5,7 @@ public:
     int subtractProductAndSum(int n) {
         int product =1,sum=0;
         while(n>0){
-            int digit = n%10;
+            int digit = n%10; // remove last digit
             product *=digit; // product
             sum += digit; // sum
             n/=10;  // remove last digit
