3627ebceb6964a8ea45c54bb4278f19bf0a6c4ff
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-09 10:45:59 +0530

    Update problem258.cpp

--- problem258.cpp
+++ problem258.cpp
@@ -1,1 +1,17 @@
 // leetCode problem number 258 - Add Digits
+class Solution {
+public:
+    int addDigits(int num) {
+        // this loop will run ultil it found the single digit
+        while ( num >= 10 || num <= -10){
+            int sum = 0;
+            // this loop will
+            while(num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+};
