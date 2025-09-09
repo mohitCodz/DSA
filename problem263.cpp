7d1d1a4c9365bd85b9aaaf4d0a83f2401af43499
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-09 11:18:19 +0530

    Update problem263.cpp

--- problem263.cpp
+++ problem263.cpp
@@ -3,7 +3,9 @@ class Solution {
 public:
     bool isUgly(int n) {
         if ( n<=0) return false;
-        while( n % 2 == 0) n /= 2;
+        while( n % 2 == 0) n /= 2; // % 2 checks if n is divisible by 2 & n /= 2 divides n by 2 
+
+.
         while( n % 3 == 0) n /= 3;
         while( n % 5 == 0) n /= 5;
 
