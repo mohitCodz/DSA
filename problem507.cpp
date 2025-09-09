b4d46fc868e52ee1080448c5fca3908917beff5a
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-09 11:57:54 +0530

    Update problem507.cpp

--- problem507.cpp
+++ problem507.cpp
@@ -1,15 +1,14 @@
 // leetCode problem 507 - Perfect Number
 class Solution {
 public:
-   bool isPerfect(int n) {
-    if (n <= 0) return false;
-    int num = 0;
-    for (int i = 1; i < n; i++) {
-        if (n % i == 0) {  // i is a divisor
-            num += i;
+    bool checkPerfectNumber(int num) {
+        if(num <= 0) return false;
+        int sum =0;
+        for(int i =1; i<num;i++){
+            if(num % i == 0){
+                sum += i;
+            }
         }
+         return sum == num;
     }
-    return num == n;
-}
-
 };
