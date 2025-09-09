085ecb1be2c7ed543e556e886d15dd30bb319b34
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-09 11:45:11 +0530

    Update problem342.cpp

--- problem342.cpp
+++ problem342.cpp
@@ -1,1 +1,11 @@
 // leetCode problem 342 - Power of four
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        while(n % 4 == 0){
+            n /= 4;
+        }
+        return n == 1;
+    }
+};
